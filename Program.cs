60a336b55170f6c7d19bc5e9dcfbd24bafba96bc
Brenna Fisher <2.bfisher@gmail.com>
2024-01-17 13:51:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Mission2Assignment;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        RollDie rd = new RollDie();
+
+        int numrolls = 0;
+        
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        numrolls = int.Parse(System.Console.ReadLine());
+
+        int[] results = rd.RollDice(numrolls);
+        int[] atrkPer = new int[13];
+
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numrolls);
+
+        for (int iCount = 2; iCount < results.Length; iCount++)
+        {
+            atrkPer[iCount] = (results[iCount] *100 / numrolls);
+            System.Console.Write((iCount) + ": ");
+
+            for (int iCount2 = 2; iCount2 < atrkPer[iCount]; iCount2++)
+            {
+                System.Console.Write("*");
+            }
+            System.Console.WriteLine();
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ RollDie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class RollDie
+    {
+        public int[] RollDice(int numrolls)
+        {
+            int[] results = new int[13];
+
+            int nRolls = numrolls;
+
+            int die1 = 0;
+            int die2 = 0;
+            int diecombo = 0;
+
+            Random random = new Random();
+
+            for (int count = 0; count < nRolls; count++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+
+                diecombo = die1 + die2;
+
+                results[diecombo]++;
+            }
+
+            return results;
+
+        }
+    }
+}
